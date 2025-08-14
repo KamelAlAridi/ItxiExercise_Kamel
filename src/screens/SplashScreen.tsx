af4f87c462754f2936dd34ab9de71f8270a4c8a6
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-14 13:12:29 +0300

    fixed return'

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,9 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 
 export default function SplashScreen() {
-  <View style={styles.container}>
-    <Text style={styles.title}>Splash Screen</Text>
-  </View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Splash Screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
