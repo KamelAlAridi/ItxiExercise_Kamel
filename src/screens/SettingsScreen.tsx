5a20f8e6decbef474f5fdd6db3039efbd7c7cfb3
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-15 21:08:44 +0300

    removed not used imports from settings screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {SettingsStackParams} from '../types/types';
