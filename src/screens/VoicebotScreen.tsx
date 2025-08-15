68e74a67bc1655c369913435a181a1fe3b8c972e
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-15 20:38:47 +0300

    added design to voice bot screen

--- src/screens/VoicebotScreen.tsx
+++ src/screens/VoicebotScreen.tsx
@@ -1,10 +1,46 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {getDBConnection, getVoice} from '../services/database';
+import GradientCircles from '../components/GradientCircles';
 
 export default function VoicebotScreen() {
+  const [voice, setVoice] = useState<string>('');
+
+  useEffect(() => {
+    const loadVoice = async (): Promise<void> => {
+      const db = await getDBConnection();
+      const voice = await getVoice(db);
+      setVoice(voice);
+    };
+    loadVoice();
+  }, []);
+
   return (
-    <View>
-      <Text>VoicebotScreen</Text>
+    <View style={styles.container}>
+      <GradientCircles />
+      <View style={styles.content}>
+        <Text style={styles.title}>Voice used is: {voice}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9FAFB',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '300',
+    color: '#2D3748',
+    marginBottom: 16,
+    letterSpacing: 1.2,
+  },
+});
