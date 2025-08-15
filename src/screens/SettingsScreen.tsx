362ca97beaecb12a1b4209fe7049475407ec8dde
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-15 21:04:13 +0300

    added design to settings screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,22 +1,39 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {SettingsStackParams} from '../types/types';
+import GradientCircles from '../components/GradientCircles';
+import GradientButton from '../components/GradientButton';
 
 type Props = NativeStackScreenProps<SettingsStackParams, 'Settings'>;
 
 export default function SettingsScreen({navigation}: Props) {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-      <Button
-        title="Set Company Id"
-        onPress={() => navigation.navigate('SetCompanyId')}
-      />
-      <Button
-        title="Pick Voice"
-        onPress={() => navigation.navigate('PickVoice')}
-      />
+    <View style={styles.container}>
+      <GradientCircles />
+      <View style={styles.content}>
+        <GradientButton
+          text="Set Company Id"
+          onpress={() => navigation.navigate('SetCompanyId')}
+        />
+        <GradientButton
+          text="Pick Voice"
+          onpress={() => navigation.navigate('PickVoice', {fromSettings: true})}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9FAFB',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+  },
+});
